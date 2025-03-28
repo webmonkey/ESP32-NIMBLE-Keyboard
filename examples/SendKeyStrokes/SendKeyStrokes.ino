3dc1a1f77de54699ac247490267cc4fb8cea524f
Berg0612 <57005514+Berg0162@users.noreply.github.com>
2025-03-28 11:45:26 +0100

    Update SendKeyStrokes.ino

--- examples/SendKeyStrokes/SendKeyStrokes.ino
+++ examples/SendKeyStrokes/SendKeyStrokes.ino
@@ -1,13 +1,13 @@
 /**
- * This example turns the ESP32 into a Bluetooth LE keyboard that writes the words, presses Enter, presses a media key and then Ctrl+Alt+Delete
+ * This example turns the ESP32 into a NimBLE keyboard that writes the words, presses Enter, presses a media key and then Ctrl+Alt+Delete
  */
-#include <BleKeyboard.h>
+#include <NimBleKeyboard.h>
 
 BleKeyboard bleKeyboard;
 
 void setup() {
   Serial.begin(115200);
-  Serial.println("Starting BLE work!");
+  Serial.println("Starting NimBLE work!");
   bleKeyboard.begin();
 }
 
